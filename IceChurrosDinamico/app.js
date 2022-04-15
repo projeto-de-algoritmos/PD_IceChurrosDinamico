adb3f0ee671da0ab6c964a3ccd1d18d29f51592e
Daniel Alves <danielribeiroalves0@gmail.com>
2022-04-15 15:23:47 -0300

    Arrumando response do algorítimo

--- IceChurrosDinamico/app.js
+++ IceChurrosDinamico/app.js
@@ -1,11 +1,11 @@
 const maiorSubSeq = require('./MaiorSubSeq/maiorSubSeq.js');
-const maiorSubSeqTest = require('./MaiorSubSeq/longestIncreasingSubsequence.js');
 
 const express = require('express');
 
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+let arr = [];
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -18,23 +18,15 @@ app.get('', (req, res) => {
 });
 
 app.post('/addValor', (req, res) => {
-    console.log(req.body)
+    arr.push(req.body.valor)
     res.render('home', { showResult: false });
 });
 
-let arr = [10, 22, 9, 33, 21, 50, 41, 60]
-let n = arr.length;
-var subSeqTest = new maiorSubSeqTest();
-teste = subSeqTest.lis(arr, n);
-console.log(teste)
 app.post('/calculaSequencia', (req, res) => {
-    res.render('home', { showResult: true, subSeqResult: test });
+    var subSeq = new maiorSubSeq();
+    result = subSeq.maiorSeq2(arr.length, arr);
+    res.render('home', { showResult: true, subSeqResult: result });
 });
-
-var subSeq = new maiorSubSeq();
-var vet = [2, 3, 14, 5, 9, 8, 4];
-teste = subSeq.maiorSeq2(vet.length, vet);
-console.log(teste);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
